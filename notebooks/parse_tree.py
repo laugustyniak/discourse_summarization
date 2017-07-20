4526e55b507cb9a4ef392f9aa3323df7b2c44459
Łukasz Augustyniak <luk.augustyniak@gmail.com>
2017-07-21 03:08:45 +0800

    relation extraction for rules added

--- notebooks/parse_tree.py
+++ notebooks/parse_tree.py
@@ -47,54 +47,63 @@ class EDUTreeRulesExtractor(object):
     def __init__(self):
         self.rules = []
         self.accepted_edus = None
-        self.relation = None
-        self.left_child = None
-        self.right_child = None
+        self.left_child_parent = None
+        self.right_child_parent = None
+        self.visited = []
 
-    def _process_tree(self, tree, relation):
+    def _process_tree(self, tree):
         for child_index, child in enumerate(tree):
-            if not child_index:
-                self.left_child = child
             # go to the subtree left child_index = 0, or right child_index = 1
+            if not child_index:
+                self.left_child_parent = tree
+            else:
+                self.right_child_parent = tree
             # check if child is leaf or subtree
             if isinstance(child, ParseTree):
                 # go into subtree
-                self._process_tree(child, relation)
+                self._process_tree(child)
             # recursively until leaf
             else:
-                relation = tree.node
+                # relation = tree.node
                 # leaf, parent/current subtree, child_index of leaf in the tree, relation type of tree/parent
-                self._traverse_parent(child, tree, child_index, relation)
+                self._traverse_parent(child, tree, child_index)
 
-    def _traverse_parent(self, leaf, parent, child_index, relation):
+    def _traverse_parent(self, leaf, parent, child_index):
         """ we reached leaf and want to parse sibling of leaf """
         # leaf = child
         if parent is not None:
             # get sibling of leaf or in false you got the same leaf
-            if child_index < len(parent):
-                # for i in range(child_index + 1, len(parent)):
-                self.__make_rules(leaf, parent[0], relation)
+            for index in range(child_index + 1, len(parent)):
+                # if parent[1] not in self.visited:
+                self.right_child_parent = parent
+                self.__make_rules(leaf, parent[index])
+                self.visited.append(parent[index])
 
             # go up in the tree
             if parent.parent is not None:
                 self.relation = parent.node
-                self._traverse_parent(leaf, parent.parent, parent.parent_index, relation)
+                self._traverse_parent(leaf, parent.parent, parent.parent_index)
 
-    def __make_rules(self, leaf_left, tree, relation):
+    def __make_rules(self, leaf_left, tree):
 
         # int oznacza, że rekursywnie doszliśmy do liścia w drzewie pod aktualnym liście analizowanym
         if isinstance(tree, int):
             if tree in self.accepted_edus and leaf_left in self.accepted_edus:
                 # find common nearest parent
+                # take relation from biger parse tree
+                if self.left_child_parent.height() > self.right_child_parent.height():
+                    relation = self.left_child_parent.node
+                else:
+                    relation = self.right_child_parent.node
                 self.rules.append((leaf_left, tree, relation))
+        # do deeper into tree
         else:
             for index, child in enumerate(tree):
-                self.__make_rules(leaf_left, child, tree.node)
+                self.__make_rules(leaf_left, child)
 
     def extract(self, tree, accepted_edus):
         self.accepted_edus = accepted_edus
-        print 'root node', tree.node
-        self._process_tree(tree, tree.node)
+        self._process_tree(tree)
 
         return self.rules
 
@@ -107,7 +116,8 @@ for doc_number in document_info_at_least_2_aspects_accepted.keys()[11:]:
     document_info = load_serialized(join(data_path, 'documents_info'))[doc_number]
 #     print(document_info)
     link_tree = load_serialized(join(data_path_link_tree, str(doc_number)))
-#     print(link_tree)
+    print(link_tree)
     print('====='*20)
     extracted_rules = rules_extractor.extract(link_tree, document_info['accepted_edus'])
+    print(extracted_rules)
     break
