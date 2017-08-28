32e682e16b0aabf0509a7d48eefc86e628a40102
Łukasz Augustyniak <luk.augustyniak@gmail.com>
2017-08-28 16:21:43 +0200

    run analysis for all reviews

---
+++ run_all_reviews.py
@@ -0,0 +1,18 @@
+import logging
+from glob import glob
+
+from run import AspectAnalysisSystem
+
+reviews = glob('data/reviews/*.json')
+
+log = logging.getLogger(__name__)
+
+for review in reviews:
+    log.info('Review file {}'.format(review))
+    AAS = AspectAnalysisSystem(input_path=review,
+                               output_path='results',
+                               gold_standard_path=None,
+                               jobs=7)
+    AAS.run()
+
+# python run.py -input data/reviews/ipod.json -output results -batch 1000 -p 23
