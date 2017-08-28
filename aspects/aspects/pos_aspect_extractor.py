9d0c321c007bbd8c1fbf72d2b4c390e2dcf04991
Łukasz Augustyniak <luk.augustyniak@gmail.com>
2017-08-28 15:37:12 +0200

    pep8 and reformatting

--- aspects/aspects/pos_aspect_extractor.py
+++ aspects/aspects/pos_aspect_extractor.py
@@ -1,57 +1,58 @@
 # -*- coding: utf-8 -*-
-# author: Krzysztof xaru Rajda
-
-import spacy
 
 
-class POSAspectExtractor:
-    def __isInterestingMain(self, token):
+class POSAspectExtractor(object):
+    def _is_interesting_main(self, token):
         return token['pos'] == 'NOUN'
 
-    def __isInterestingAddition(self, token):
-        return token['pos'] == 'ADV' or token['pos'] == 'NUM' or token['pos'] == 'NOUN' or token['pos'] == 'ADJ'
+    def _is_interesting_addition(self, token):
+        return token['pos'] == 'ADV' or token['pos'] == 'NUM' \
+               or token['pos'] == 'NOUN' or token['pos'] == 'ADJ'
 
-    def extract(self, inputData):
+    def extract(self, input_data):
 
-        tokens = inputData['tokens']
+        tokens = input_data['tokens']
 
         aspects = []
-        aspectSequence = []
-        aspectSequenceMainEncountered = False
-        aspectSequenceEnabled = False
+        aspect_sequence = []
+        aspect_sequence_main_encountered = False
+        aspect_sequence_enabled = False
 
         # dla każdego tokena tekstu
-        for id, result in enumerate(tokens):
+        for idx, result in enumerate(tokens):
 
             # jesli jest główny (rzeczownik) - akceptujemy od razu
-            if self.__isInterestingMain(result):
+            if self._is_interesting_main(result):
                 if not result['is_stop']:
-                    aspectSequence.append(result['text'])
-                aspectSequenceEnabled = True
-                aspectSequenceMainEncountered = True
+                    aspect_sequence.append(result['text'])
+                aspect_sequence_enabled = True
+                aspect_sequence_main_encountered = True
 
-            # jesli jest ciekawy (przymiotnik, przysłówek, liczba) i jest potencjalnym elementem sekwencji - dodajemy
-            elif self.__isInterestingAddition(result) and (
-                        (id + 1 < len(tokens) and self.__isInterestingAddition(tokens[id + 1])) or id + 1 == len(
+            # jesli jest ciekawy (przymiotnik, przysłówek, liczba) i jest
+            # potencjalnym elementem sekwencji - dodajemy
+            elif self._is_interesting_addition(result) and (
+                        (idx + 1 < len(
+                            tokens) and self._is_interesting_addition(
+                            tokens[idx + 1])) or idx + 1 == len(
                         tokens)):
                 if not result['is_stop']:
-                    aspectSequence.append(result['text'])
+                    aspect_sequence.append(result['text'])
             else:
 
                 # akceptujemy sekwencje, jesli byl w niej element główny
-                if aspectSequenceEnabled and aspectSequenceMainEncountered:
-                    aspect = ' '.join(aspectSequence)
+                if aspect_sequence_enabled and aspect_sequence_main_encountered:
+                    aspect = ' '.join(aspect_sequence)
 
-                    if not aspect in aspects:
+                    if aspect not in aspects:
                         aspects.append(aspect)
 
-                aspectSequenceMainEncountered = False
-                aspectSequenceEnabled = False
-                aspectSequence = []
+                aspect_sequence_main_encountered = False
+                aspect_sequence_enabled = False
+                aspect_sequence = []
 
         # dodajemy ostatnią sekwencje
-        if aspectSequenceEnabled and aspectSequenceMainEncountered:
-            aspects.append(' '.join(aspectSequence))
+        if aspect_sequence_enabled and aspect_sequence_main_encountered:
+            aspects.append(' '.join(aspect_sequence))
 
         # nie wiem czemu puste wartosci leca - odfiltrowujemy
         aspects = [x for x in aspects if len(x) > 0]
