1032c67c7815efc3485db57e33ab76a006f04222
Łukasz Augustyniak <luk.augustyniak@gmail.com>
2017-10-17 00:44:31 +0200

    added some statistics for aspect extension with sentiment

--- aspects/results_analysis/gerani_graph_analysis.py
+++ aspects/results_analysis/gerani_graph_analysis.py
@@ -5,6 +5,10 @@ log = logging.getLogger(__name__)
 
 
 def get_dir_moi_for_node(graph, aspects_per_edu, documents_info):
+    n_skipped_edus = 0
+    n_aspects_not_in_graph = 0
+    n_aspects_updated = 0
+    n_all_documents = len(documents_info)
     aspects_per_edu_sentiment = []
     for doc_info in documents_info.itervalues():
         for edu_id, sentiment in doc_info['sentiment'].iteritems():
@@ -12,7 +16,10 @@ def get_dir_moi_for_node(graph, aspects_per_edu, documents_info):
                 for aspect in aspects_per_edu[edu_id]:
                     aspects_per_edu_sentiment.append((aspect, sentiment))
             except KeyError as err:
-                log.info('There is not aspect: {} in graph'.format(str(err)))
+                n_skipped_edus += 1
+                log.info(
+                    'Aspect: {} not extracted from edu: {}'.format(str(err),
+                                                                   edu_id))
 
     aspect_sentiments = dict()
     for k, v in aspects_per_edu_sentiment:
@@ -22,7 +29,14 @@ def get_dir_moi_for_node(graph, aspects_per_edu, documents_info):
         try:
             graph.node[aspect]['sentiment'] = sentiments
             graph.node[aspect]['dir_moi'] = np.sum([x ** 2 for x in sentiments])
+            n_aspects_updated += 1
         except KeyError as err:
+            n_aspects_not_in_graph += 1
             log.info('There is not aspect: {} in graph'.format(str(err)))
+
+    log.info('#{} skipped aspects out of #{} documents'.format(n_skipped_edus,
+                                                               n_all_documents))
+    log.info('#{} aspects not in graph'.format(n_aspects_not_in_graph))
+    log.info('#{} aspects updated in graph'.format(n_aspects_updated))
 
     return graph
