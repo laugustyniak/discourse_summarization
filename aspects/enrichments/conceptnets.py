e0edb967a8f2a346925a8960f6a878fa8feb1751
Łukasz Augustyniak <luk.augustyniak@gmail.com>
2018-03-16 01:13:09 +0100

    style fix

--- aspects/enrichments/conceptnets.py
+++ aspects/enrichments/conceptnets.py
@@ -13,11 +13,9 @@ log = logging.getLogger(__name__)
 class Sentic(object):
     def __init__(self):
         self.sentic_df = pd.DataFrame.from_dict(senticnet4, orient='index')
-        self.sentic_df.columns = ['pleasantness', 'attention', 'sensivity',
-                                  'aptitude', 'modtag1', 'modtag2',
+        self.sentic_df.columns = ['pleasantness', 'attention', 'sensivity', 'aptitude', 'modtag1', 'modtag2',
                                   'polarity_value', 'polarity_intensity',
-                                  'semantics1', 'semantics2', 'semantics3',
-                                  'semantics4', 'semantics5']
+                                  'semantics1', 'semantics2', 'semantics3', 'semantics4', 'semantics5']
 
     def get_concept_from_senticnet_by_partname(self, partname):
         """
@@ -26,8 +24,7 @@ class Sentic(object):
         :param partname: concept that will be filtered
         :return: Data Frame with concepts
         """
-        return self.sentic_df[
-            self.sentic_df.index.str.contains(partname)].sort_index()
+        return self.sentic_df[self.sentic_df.index.str.contains(partname)].sort_index()
 
     def get_semantic_concept_by_concept(self, partname, exact_match=False):
         """
@@ -45,19 +42,13 @@ class Sentic(object):
             values: list of related concepts
         """
         concepts = {}
-        semantic_col_name = ['semantics1', 'semantics2', 'semantics3',
-                             'semantics4', 'semantics5']
+        semantic_col_name = ['semantics1', 'semantics2', 'semantics3', 'semantics4', 'semantics5']
         if exact_match:
-            df = self.sentic_df[
-                self.sentic_df.index == partname].sort_index()
+            df = self.sentic_df[self.sentic_df.index == partname].sort_index()
         else:
-            df = self.sentic_df[
-                self.sentic_df.index.str.contains(partname)].sort_index()
+            df = self.sentic_df[self.sentic_df.index.str.contains(partname)].sort_index()
         for row in df.iterrows():
-            related_concepts = []
-            for col in semantic_col_name:
-                related_concepts.append(row[1][col])
-            concepts[row[0]] = related_concepts
+            concepts[row[0]] = [row[1][col] for col in semantic_col_name]
         return concepts
 
 
@@ -65,4 +56,5 @@ class Sentic(object):
 def load_conceptnet_io():
     log.info('ConceptNet.io temp files will be load from: {}'.format(settings.CONCEPTNET_IO_PKL))
     with open(settings.CONCEPTNET_IO_PKL.as_posix(), 'rb') as f:
-        return pickle.load(f)
+        conceptnet_io = pickle.load(f)
+    return conceptnet_io
