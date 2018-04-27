0eb96370c5230fb50965ecde530d55a971a5026d
Łukasz Augustyniak <luk.augustyniak@gmail.com>
2018-04-27 20:16:11 +0200

    update run
    drop gexf

--- run.py
+++ run.py
@@ -438,7 +438,6 @@ class AspectAnalysisSystem:
         logging.info('Save graph with Gephi suitable extension')
         aspects_graph = self.serializer.load(self.paths.aspects_graph)
         nx.write_gpickle(aspects_graph, self.paths.aspects_graph_gpkl)
-        nx.write_gexf(aspects_graph, self.paths.aspects_graph_gexf)
 
 
 if __name__ == "__main__":
--- run_specific_dataset.py
+++ run_specific_dataset.py
@@ -7,15 +7,20 @@ from run import AspectAnalysisSystem
 
 log = logging.getLogger(__name__)
 
-dataset_path = settings.BING_LIU_LINKSYS_ROUTER.as_posix()
+DATASET_PATH = settings.BING_LIU_IPOD.as_posix()
+N_JOBS = multiprocessing.cpu_count() - 1
 
 AAS = AspectAnalysisSystem(
-    input_path=dataset_path,
-    output_path=join('results', basename(dataset_path).replace('.json', '')),
+    input_path=DATASET_PATH,
+    output_path=join('results', basename(DATASET_PATH).replace('.json', '')),
     gold_standard_path=None,
     analysis_results_path=None,
-    jobs=multiprocessing.cpu_count() - 1,
+    jobs=N_JOBS,
     batch_size=50,
+    neutral_sent=True,
+    cycle_in_relations=False,
+    filter_gerani=False,
+    aht_gerani=False,
     # max_docs=15000,
 )
 
