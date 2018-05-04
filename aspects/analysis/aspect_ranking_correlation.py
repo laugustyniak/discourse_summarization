b9ccdc5699d5730bc80f4e7f4f01edac91365fa8
Łukasz Augustyniak <luk.augustyniak@gmail.com>
2018-05-04 17:20:03 +0200

    ranking build on freq and rst lookup posistions

--- aspects/analysis/aspect_ranking_correlation.py
+++ aspects/analysis/aspect_ranking_correlation.py
@@ -1,10 +1,9 @@
 from os.path import basename
 
 from scipy.stats import stats
-from sklearn import preprocessing
 
 from aspects.analysis.statistics_bing_liu import get_aspect_frequency_ranking
-from aspects.analysis.statistics_rst_graphs import get_aspect_ranking_based_on_rst_and_pagerank, ASPECTS_GRAPH_PATHS
+from aspects.analysis.statistics_rst_graphs import get_aspects_rankings_from_rst, ASPECTS_GRAPH_PATHS
 from aspects.utilities import settings
 
 
@@ -12,7 +11,6 @@ def get_spearman_and_kendalltau_correlations():
     correlations = {}
 
     for reviews_path in settings.ALL_BING_LIU_REVIEWS_PATHS:
-        label_encoder = preprocessing.LabelEncoder()
         dataset_name = basename(reviews_path).split('.')[0]
         print(f'Dataset to analyze: {dataset_name}')
 
@@ -21,29 +19,26 @@ def get_spearman_and_kendalltau_correlations():
         print(aspects_freq_manual_assignment)
 
         # get aspects from RST + PageRank
-        aspects_from_rst_based_on_pagerank = get_aspect_ranking_based_on_rst_and_pagerank(
+        aspects_from_rst_based_on_pagerank = get_aspects_rankings_from_rst(
             [
                 aspects_graph_path
                 for aspects_graph_path
                 in ASPECTS_GRAPH_PATHS
                 if dataset_name in aspects_graph_path
-            ][0]
+            ][0],
+            aspects_freq_manual_assignment
         )
         print(aspects_from_rst_based_on_pagerank)
 
-        # merge aspects from both sources
-        all_aspects_from_both_datasets_rankings = aspects_freq_manual_assignment + aspects_from_rst_based_on_pagerank
-
-        label_encoder.fit(all_aspects_from_both_datasets_rankings)
-        aspects_freq_manual_assignment_vector = label_encoder.transform(aspects_freq_manual_assignment)
-        aspects_from_rst_based_on_pagerank_vector = label_encoder.transform(aspects_from_rst_based_on_pagerank)
+        aspects_freq_manual_assignment_ranking, aspects_from_rst_based_on_pagerank_ranking = create_rankings(
+            aspects_freq_manual_assignment, aspects_from_rst_based_on_pagerank)
 
         spearman_correlation = stats.spearmanr(
-            aspects_freq_manual_assignment_vector, aspects_from_rst_based_on_pagerank_vector)
+            aspects_freq_manual_assignment_ranking, aspects_from_rst_based_on_pagerank_ranking)
         print(f'{dataset_name}, Spearman correlation of ranking: {spearman_correlation}')
 
         kendalltau_correlation = stats.kendalltau(
-            aspects_freq_manual_assignment_vector, aspects_from_rst_based_on_pagerank_vector)
+            aspects_freq_manual_assignment_ranking, aspects_from_rst_based_on_pagerank_ranking)
         print(f'{dataset_name}, KendalTau correlation of ranking: {kendalltau_correlation}')
 
         correlations[dataset_name] = {
@@ -53,6 +48,19 @@ def get_spearman_and_kendalltau_correlations():
             'Kendall Tau p-value': kendalltau_correlation[1]
         }
     return correlations
+
+
+def create_rankings(aspects_1, aspects_2):
+    aspects_rank = {aspect: idx for idx, aspect in enumerate(aspects_1, start=1)}
+    aspects_1_ranking = list(aspects_rank.values())
+
+    aspects_2_ranking = [
+        aspect
+        for aspect
+        in aspects_2
+    ]
+
+    return aspects_1_ranking, aspects_2_ranking
 
 
 if __name__ == '__main__':
--- aspects/analysis/statistics_rst_graphs.py
+++ aspects/analysis/statistics_rst_graphs.py
@@ -23,9 +23,10 @@ ASPECTS_GRAPH_PATHS = [
     in ASPECTS_GRAPH_PATHS
 ]
 
+AspectPageRank = namedtuple('Aspect_PageRank', 'aspect, pagerank')
+
 
 def get_aspect_ranking_based_on_rst_and_pagerank(aspects_graph_path, top_n=10):
-    AspectPageRank = namedtuple('Aspect_PageRank', 'aspect, pagerank')
     aspect_graph = nx.read_gpickle(aspects_graph_path)
 
     df = pd.DataFrame(
@@ -37,6 +38,33 @@ def get_aspect_ranking_based_on_rst_and_pagerank(aspects_graph_path, top_n=10):
     )
     df.sort_values('pagerank', ascending=False, inplace=True)
     return list(df.aspect.head(top_n))
+
+
+def get_aspects_rankings_from_rst(aspects_graph_path, aspects):
+    aspect_graph = nx.read_gpickle(aspects_graph_path)
+    df = pd.DataFrame(
+        [
+            AspectPageRank(aspect=x[0], pagerank=x[1])
+            for x
+            in nx.pagerank_scipy(aspect_graph, weight='weight').items()
+        ]
+    )
+    df.sort_values('pagerank', ascending=False, inplace=True)
+    df = df.reset_index().drop(['index'], axis=1)
+    ranks = [
+        get_index_of_element_in_series(df.aspect, aspect)
+        for aspect
+        in aspects
+    ]
+    return ranks
+
+
+def get_index_of_element_in_series(series: pd.Series, element: str) -> int:
+    row = series[series == element]
+    if not row.empty:
+        return row.index[0] + 1
+    else:
+        return len(series) + 1
 
 
 if __name__ == '__main__':
