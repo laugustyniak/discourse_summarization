03545693dae4aae885ea8802df22ee459e5483d2
Łukasz Augustyniak <luk.augustyniak@gmail.com>
2018-06-08 17:06:42 +0200

    remove class from preprocesser, only methods -> simplified tests fix

--- aspects/aspects/unit_tests/test_aspect_extraction.py
+++ aspects/aspects/unit_tests/test_aspect_extraction.py
@@ -1,7 +1,7 @@
 import unittest
 
 from aspects.aspects.aspect_extractor import AspectExtractor
-from aspects.preprocessing.preprocesser import Preprocesser
+from aspects.preprocessing import preprocesser
 from aspects.utilities import settings
 
 
@@ -9,7 +9,6 @@ class AspectExtractionTest(unittest.TestCase):
 
     def test_get_aspects_nouns(self):
         aspects_expected = [u'car', u'phone']
-        preprocesser = Preprocesser()
         raw_text = u'i have car and phone'
         text = preprocesser.preprocess(raw_text)
         aspects_extractor = AspectExtractor()
@@ -19,7 +18,6 @@ class AspectExtractionTest(unittest.TestCase):
 
     def test_get_aspects_noun_phrase(self):
         aspects_expected = [u'car', u'phone']
-        preprocesser = Preprocesser()
         raw_text = u'i have a nice car and awesome phone'
         text = preprocesser.preprocess(raw_text)
         aspects_extractor = AspectExtractor()
@@ -28,7 +26,6 @@ class AspectExtractionTest(unittest.TestCase):
 
     def test_get_aspects_ner(self):
         aspects_expected = [u'angela merkel', u'europe']
-        preprocesser = Preprocesser()
         # sample text, don't correct :)
         raw_text = u'Angela Merkel is German, angela merkel is europe!'
         text = preprocesser.preprocess(raw_text)
@@ -38,7 +35,6 @@ class AspectExtractionTest(unittest.TestCase):
 
     def test_get_aspects_ner_false(self):
         aspects_expected = []
-        preprocesser = Preprocesser()
         # sample text, don't correct :)
         raw_text = u'this is the biggest in Europe!'
         text = preprocesser.preprocess(raw_text)
@@ -48,7 +44,6 @@ class AspectExtractionTest(unittest.TestCase):
 
     def test_1_char_aspects_and_donts(self):
         aspects_expected = []
-        preprocesser = Preprocesser()
         raw_text = u'I don\'t like it, it is not, do not'
         text = preprocesser.preprocess(raw_text)
         aspects_extractor = AspectExtractor()
@@ -57,7 +52,6 @@ class AspectExtractionTest(unittest.TestCase):
 
     def test_get_aspects_telecom_lower_cased(self):
         aspects_expected = [u'plan', u'sprint']
-        preprocesser = Preprocesser()
         raw_text = u'i wonder if you can propose for me better plan and ' \
                    u'encourage me to not leave for sprint i could get a ' \
                    u'better plan there'
@@ -75,7 +69,6 @@ class AspectExtractionTest(unittest.TestCase):
 
     def test_sentic_concept_extraction(self):
         concept = 'screen'
-        preprocesser = Preprocesser()
         raw_text = u'i wonder if you can propose for me better screen'
         text = preprocesser.preprocess(raw_text)
         if settings.SENTIC_ASPECTS:
@@ -86,7 +79,6 @@ class AspectExtractionTest(unittest.TestCase):
 
     def test_conceptnet_io_concept_extraction(self):
         concept = 'screen'
-        preprocesser = Preprocesser()
         raw_text = u'i wonder if you can propose for me better screen'
         text = preprocesser.preprocess(raw_text)
         if settings.CONCEPTNET_IO_ASPECTS:
@@ -105,7 +97,6 @@ class AspectExtractionTest(unittest.TestCase):
 
     def test_conceptnet_io_concept_extraction_en_filtered(self):
         concept = 'converter'
-        preprocesser = Preprocesser()
         raw_text = u'i wonder if you can propose for me better converter'
         text = preprocesser.preprocess(raw_text)
         if settings.CONCEPTNET_IO_ASPECTS:
@@ -119,7 +110,6 @@ class AspectExtractionTest(unittest.TestCase):
 
     def test_conceptnet_io_concept_extraction_en_filtered_phone(self):
         concept = 'phone'
-        preprocesser = Preprocesser()
         raw_text = u'i wonder if you can propose for me better phone'
         text = preprocesser.preprocess(raw_text)
         if settings.CONCEPTNET_IO_ASPECTS:
@@ -131,33 +121,8 @@ class AspectExtractionTest(unittest.TestCase):
                 self.assertEqual(True, settings.CONCEPTNET_IO_LANG == c['start-lang'])
                 self.assertEqual(True, settings.CONCEPTNET_IO_LANG == c['end-lang'])
 
-    def test_conceptnet_io_concept_extraction_paggination(self):
-        concept = 'phone'
-        preprocesser = Preprocesser()
-        raw_text = u'i wonder if you can propose for me better phone'
-        text = preprocesser.preprocess(raw_text)
-        if settings.CONCEPTNET_IO_ASPECTS:
-            aspects_extractor = AspectExtractor()
-            _, concepts_obtained, _ = aspects_extractor.extract(text)
-            concepts = concepts_obtained['conceptnet_io'][concept]
-
-            self.assertGreater(len(concepts), 20)
-
-    def test_conceptnet_io_concept_extraction_paggination_same_concepts(self):
-        concept = 'device'
-        preprocesser = Preprocesser()
-        raw_text = u'this device is really good device, but this phone'
-        text = preprocesser.preprocess(raw_text)
-        if settings.CONCEPTNET_IO_ASPECTS:
-            aspects_extractor = AspectExtractor()
-            _, concepts_obtained, _ = aspects_extractor.extract(text)
-            concepts = concepts_obtained['conceptnet_io'][concept]
-
-            self.assertGreater(len(concepts), 20)
-
     def test_keyword_aspect_extraction(self):
         keywords_expected_rake = [(u'propose', 1.0), (u'screen', 1.0)]
-        preprocesser = Preprocesser()
         raw_text = u'i wonder if you can propose for me better screen'
         text = preprocesser.preprocess(raw_text)
         aspects_extractor = AspectExtractor()
