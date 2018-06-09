4f3d35e9e7c4453fc853ac37d08dc77fbf27914a
Łukasz Augustyniak <luk.augustyniak@gmail.com>
2018-06-09 22:55:49 +0200

    cpikle to pickle

--- aspects/utilities/transformations.py
+++ aspects/utilities/transformations.py
@@ -1,13 +1,13 @@
-from os.path import join
-import cPickle
 import itertools
+import pickle
 from collections import defaultdict
+from os.path import join
 
 
 def load_serialized(f_path):
     """ loads pickled files from specified path """
     with open(join(f_path)) as f:
-        return cPickle.load(f)
+        return pickle.load(f)
 
 
 def flatten_list(list2d):
@@ -19,6 +19,6 @@ def flatten_list(list2d):
 def merge_dicts_by_key(*args):
     merged_dict = defaultdict(list)
     for d in args:
-        for key, value in d.iteritems():
+        for key, value in d.items():
             merged_dict[key].append(value)
     return merged_dict
