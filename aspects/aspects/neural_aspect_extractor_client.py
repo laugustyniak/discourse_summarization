c7693542b7a9736a88f2fa2442420aab3613f8f8
Łukasz <luk.augustyniak@gmail.com>
2019-04-23 18:50:27 +0200

    clean up

--- aspects/aspects/neural_aspect_extractor_client.py
+++ aspects/aspects/neural_aspect_extractor_client.py
@@ -4,8 +4,10 @@ from flask.json import jsonify
 
 class NeuralAspectExtractorClient:
 
+    def __init__(self, url=None, json_request_key=None, json_response_key=None):
+        self.url = url or "http://localhost:5000/api/aspects/extract"
+        self.json_request_key = json_request_key or 'text'
+        self.json_response_key = json_response_key or 'aspects'
+
     def extract(self, text):
-        return jsonify(requests.post(
-            "http://localhost:5000/api/aspects/extract",
-            data={'text': text}
-        ))['aspects']
+        return jsonify(requests.post(self.url, data={self.json_request_key: text}))[self.json_response_key]
