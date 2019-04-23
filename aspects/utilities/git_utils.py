47cd9e16caea77b2c62ce8cee6c77717fac10932
Łukasz <luk.augustyniak@gmail.com>
2019-04-23 11:47:04 +0200

    bytes to str

--- aspects/utilities/git_utils.py
+++ aspects/utilities/git_utils.py
@@ -2,8 +2,8 @@ import subprocess
 
 
 def get_git_revision_hash():
-    return subprocess.check_output(['git', 'rev-parse', 'HEAD'])
+    return subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode("utf-8").strip()
 
 
 def get_git_revision_short_hash():
-    return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'])
+    return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode("utf-8").strip()
