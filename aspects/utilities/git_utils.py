ff2cbf4f6e8676ecf851e5d8a3cc0a0bea033690
Łukasz <luk.augustyniak@gmail.com>
2019-04-23 11:08:54 +0200

    add git hash getter

---
+++ aspects/utilities/git_utils.py
@@ -0,0 +1,9 @@
+import subprocess
+
+
+def get_git_revision_hash():
+    return subprocess.check_output(['git', 'rev-parse', 'HEAD'])
+
+
+def get_git_revision_short_hash():
+    return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'])
