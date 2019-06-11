a31c5f1b89c4cc6a3a4555cd73f57d5042a05a81
Łukasz <luk.augustyniak@gmail.com>
2019-06-11 12:43:49 +0200

    semeval get aspects and entities

--- aspects/analysis/semeval_2016_parser.py
+++ aspects/analysis/semeval_2016_parser.py
@@ -1,8 +1,8 @@
-from collections import Counter
 from pathlib import Path
 
 import pandas as pd
 import seaborn as sns
+from cytoolz.itertoolz import pluck
 from lxml import etree as ET
 
 from utilities.settings import SEMEVAL_DATASETS_2016
@@ -10,8 +10,9 @@ from utilities.settings import SEMEVAL_DATASETS_2016
 sns.set(style="whitegrid")
 
 semeval_files = Path(SEMEVAL_DATASETS_2016).glob('*.xml*')
+dfs = []
 
-for semeval_file in list(semeval_files)[:1]:
+for semeval_file in list(semeval_files):
     tree = ET.parse(semeval_file.as_posix())
     root = tree.getroot()
 
@@ -22,24 +23,29 @@ for semeval_file in list(semeval_files)[:1]:
 
     opinions = root.findall("**/**/Opinion")
     categories = [opinion.attrib["category"] for opinion in opinions]
+    entities_and_aspects = [cat.split('#') for cat in categories]
     polarities = [opinion.attrib["polarity"] for opinion in opinions]
     print("# Opinions  : ", len(opinions))
-    df = pd.DataFrame({"categories": categories, "polarities": polarities}).sort_values("categories")
-    categories = Counter(df.categories)
-    print("# Categories: ", len(categories))
+    df = pd.DataFrame({
+        "category": categories,
+        "entity": list(pluck(0, entities_and_aspects)),
+        'aspect': list(pluck(1, entities_and_aspects)),
+        "polarity": polarities
+    }).sort_values("entity")
+    dfs.append(df)
 
-    g = sns.countplot(y="categories", hue="polarities", data=df,
-                      palette={"neutral": "yellow", "negative": "#FF1744", "positive": "#00E676"})
+df_all = pd.concat(dfs)
+df_all.polarity = df_all.polarity.apply(lambda t: t if t else 'unknown')
+df_all.to_csv(SEMEVAL_DATASETS_2016 / 'all-entities-and-aspects.csv')
 
-    # sns.plt.tight_layout()
-    # sns.plt.savefig(join("images", "1.png"))
-    # sns.plt.show()
-    #
-    # template = Template(open(join("report", "README.md.tmp")).read())
-    # data = {}
-    # data["num_reviews"] = len(reviews)
-    # data["num_sentences"] = len(sentences)
-    # data["num_opinions"] = len(opinions)
-    # data["num_categories"] = len(categories)
-    # content = template.render(data=data)
-    # open("README.md", "w").write(content)
+g = sns.countplot(
+    y="category",
+    hue="polarity",
+    data=df_all,
+    palette={
+        "neutral": "yellow",
+        "negative": "#FF1744",
+        "positive": "#00E676",
+        "unknown": "grey"
+    }
+)
