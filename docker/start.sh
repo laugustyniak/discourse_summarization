3bc958a9b564613e0126170de26d0f6b0e65c222
Łukasz <luk.augustyniak@gmail.com>
2019-11-02 03:59:46 +0100

    docker for RST

--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# old version of inline docker run, after building dockerfile as name sa
-# docker run -d -p 27:22 -p 8778:8888 -e "PASSWORD=SAa2015" -v /datasets:/datasets -v /home/lukasz:/notebooks -v /nfs:/nfs  sa
-docker run -d -p 27:22 -p 8778:8888 -e "PASSWORD=SAa2015" -v /home/lukasz:/notebooks sa
+docker run -dt -p 27:22 -p 8778:8888 -e "PASSWORD=SAa2015" -v /home/laugustyniak:/notebooks -v /datasets:/datasets -v /nfs:/nfs docker_sa
 
