bbd246277326d7bfbf8e203e11fa8ec8f3bbce5d
Łukasz <luk.augustyniak@gmail.com>
2019-11-11 21:25:08 +0100

    flask app udpate

--- docker/flask_app.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-export FLASK_APP=/app/rhetorical_parser/src/app.py
-flask run -h 0.0.0.0
--- docker/src/app.py
+++ docker/src/app.py
@@ -17,7 +17,7 @@ def extract_aspects():
     tree_temp_name = str(uuid.uuid4())
     text = request.json['text']
     response = {
-        'aspects': parser.parse(tree_temp_name, parse_text=text)
+        'tree': parser.parse(tree_temp_name, parse_text=text)
     }
     rmtree(TREE_TEMP_DIR + tree_temp_name)
     return jsonify(response)
@@ -25,4 +25,4 @@ def extract_aspects():
 
 if __name__ == '__main__':
     parser = DiscourseParser(output_dir=TREE_TEMP_DIR)
-    app.run()
+    app.run(debug=True, host='0.0.0.0')
