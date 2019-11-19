f6fda69bdbc9d442627cb20c9f0f84596d6e92ea
Łukasz <luk.augustyniak@gmail.com>
2019-11-19 09:27:14 +0100

    iterate over sentences

--- docker/src/prep/preprocesser.py
+++ docker/src/prep/preprocesser.py
@@ -3,22 +3,20 @@ Created on 2014-01-18
 
 @author: Wei
 '''
-import os.path
 import re
-import subprocess
-from os.path import join
 
-import paths
+import spacy
+
+import prep_utils
 from document.dependency import Dependency
 from document.sentence import Sentence
 from document.token import Token
-from trees.lexicalized_tree import LexicalizedTree
-
-import prep_utils
 from syntax_parser import SyntaxParser
+from trees.lexicalized_tree import LexicalizedTree
 
 
 class Preprocesser:
+
     def __init__(self):
         self.syntax_parser = None
 
@@ -28,57 +26,16 @@ class Preprocesser:
             raise e
 
         self.max_sentence_len = 100
-
-    def heuristic_sentence_splitting(self, raw_sent):
-        if len(raw_sent) == 0:
-            return []
-
-        if len(raw_sent.split()) <= self.max_sentence_len:
-            return [raw_sent]
-
-        i = len(raw_sent) / 2
-        j = i
-        k = i + 1
-        boundaries = [';', ':', '!', '?']
-
-        results = []
-        while j > 0 and k < len(raw_sent) - 1:
-            if raw_sent[j] in boundaries:
-                l_sent = raw_sent[: j + 1]
-                r_sent = raw_sent[j + 1:].strip()
-
-                if len(l_sent.split()) > 1 and len(r_sent.split()) > 1:
-                    results.extend(self.heuristic_sentence_splitting(l_sent))
-                    results.extend(self.heuristic_sentence_splitting(r_sent))
-                    return results
-                else:
-                    j -= 1
-                    k += 1
-            elif raw_sent[k] in boundaries:
-                l_sent = raw_sent[: k + 1]
-                r_sent = raw_sent[k + 1:].strip()
-
-                if len(l_sent.split()) > 1 and len(r_sent.split()) > 1:
-                    results.extend(self.heuristic_sentence_splitting(l_sent))
-                    results.extend(self.heuristic_sentence_splitting(r_sent))
-                    return results
-                else:
-                    j -= 1
-                    k += 1
-            else:
-                j -= 1
-                k += 1
-
-        if len(results) == 0:
-            return [raw_sent]
+        self.nlp = spacy.load('en')
 
     def parse_single_sentence(self, raw_text):
         return self.syntax_parser.parse_sentence(raw_text)
 
-    def process_single_sentence(self, doc, raw_text, end_of_para):
-        sentence = Sentence(len(doc.sentences), raw_text + ('<s>' if not end_of_para else '<P>'), doc)
+    def process_single_sentence(self, doc, raw_text):
+        sentence = Sentence(len(doc.sentences), raw_text, doc)
         parse_tree_str, deps_str = self.parse_single_sentence(raw_text)
 
+        # TODO initialization of Lexicalized Trees
         parse = LexicalizedTree.parse(parse_tree_str, leaf_pattern='(?<=\\s)[^\)\(]+')
         # parse = LexicalizedTree()
         sentence.set_unlexicalized_tree(parse)
@@ -111,40 +68,10 @@ class Preprocesser:
 
         return heads
 
-    def sentence_splitting(self, raw_filename, doc):
+    def preprocess(self, text, doc):
         doc.sentences = []
-
-        cmd = 'perl "{}" -d "{}" -i "{}"'.format(
-            join(paths.SSPLITTER_PATH, 'boundary.pl'),
-            join(paths.SSPLITTER_PATH, 'HONORIFICS'),
-            os.path.abspath(raw_filename))
-
-        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-        output, errdata = p.communicate()
-
-        if len(errdata) == 0:
-            raw_paras = output.strip().split('\n\n')
-            seg_sents = []
-            for raw_string in raw_paras:
-                raw_sentences = raw_string.split('\n')
-                for (i, raw_sent) in enumerate(raw_sentences):
-                    if len(raw_sent.split()) > self.max_sentence_len:
-                        chunked_raw_sents = self.heuristic_sentence_splitting(raw_sent)
-                        if len(chunked_raw_sents) == 1:
-                            continue
-
-                        for (j, sent) in enumerate(chunked_raw_sents):
-                            seg_sents.append((sent, i == len(raw_sentences) - 1 and j == len(chunked_raw_sents)))
-                    else:
-                        seg_sents.append((raw_sent, i == len(raw_sentences) - 1))
-        else:
-            raise NameError("*** Sentence splitter crashed, with trace %s..." % errdata)
-
-        for (i, (raw_text, end_of_para)) in enumerate(seg_sents):
-            self.process_single_sentence(doc, raw_text, end_of_para)
-
-    def preprocess(self, raw_filename, doc):
-        self.sentence_splitting(raw_filename, doc)
+        for sentence in self.nlp(unicode(text)).sents:
+            self.process_single_sentence(doc, sentence.text)
 
     def unload(self):
         if self.syntax_parser:
