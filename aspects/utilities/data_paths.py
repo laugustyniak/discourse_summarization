2cb52b91a3ff360a3c16a6d23636671bbb3b17be
Łukasz <luk.augustyniak@gmail.com>
2020-02-28 22:21:03 +0100

    paths cleanup

--- aspects/utilities/data_paths.py
+++ aspects/utilities/data_paths.py
@@ -3,26 +3,16 @@ from pathlib import Path
 from typing import Union
 
 
-class IOPaths:
-    def __init__(self, input_path: Union[str, Path], output_path: Union[str, Path], suffix: str = None):
-        if suffix is None:
-            self.suffix = ''
+class ExperimentPaths:
+    def __init__(self, input_path: Union[str, Path], output_path: Union[str, Path]):
+        self.input = Path(input_path)
 
         self.output_path = Path(output_path)
         self.output_path.mkdir(exist_ok=True, parents=True)
 
-        self.input = input_path
-
         self.discourse_trees_df = self.output_path / 'discourse_trees_df.pkl'
-
-        self.aspects_graph = self.output_path / self._add_suffix('aspects_graph.pkl')
-        self.spanning_tree = self.output_path / self._add_suffix('spanning_tree.pkl')
-        self.graph_flatten = self.output_path / self._add_suffix('graph_flatten.pkl')
-        self.aspect_sentiments = self.output_path / self._add_suffix('aspect_sentiments.pkl')
-        self.aspects_weighted_page_ranks = self.output_path / self._add_suffix('aspects_page_ranks')
-
-        self.final_docs_info = self.output_path / self._add_suffix('final_documents_info')
-
-    def _add_suffix(self, name: str):
-        name = Path(name)
-        return '{}_{}{}'.format(name.stem, self.suffix, name.suffix) if self.suffix else name.name
+        self.aspect_to_aspect_graph = self.output_path / 'aspect_2_aspect_graph.pkl'
+        self.aspect_hierarchical_tree = self.output_path / 'aspect_hierarchical_tree.pkl'
+        self.aspect_to_aspect_graph_flatten = self.output_path / 'aspect_2_aspect_graph_flatten.pkl'
+        self.aspect_sentiments = self.output_path / 'aspect_sentiments.pkl'
+        self.aspects_weighted_page_ranks = self.output_path / 'aspects_weighted_page_ranks.pkl'
