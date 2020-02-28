a4766283c49c24e97dd87096101d502065a7dab3
Łukasz <luk.augustyniak@gmail.com>
2020-02-28 22:27:07 +0100

    streamlit vis wip

--- aspects/visualization/arrg.py
+++ aspects/visualization/arrg.py
@@ -5,7 +5,7 @@ import networkx as nx
 import streamlit as st
 
 from aspects.data_io.serializer import Serializer
-from aspects.utilities.data_paths import IOPaths
+from aspects.utilities.data_paths import ExperimentPaths
 
 sys.path.append('/datasets/sentiment/aspects/sentiment-backend/')
 
@@ -26,17 +26,18 @@ results_dir_name = st.sidebar.selectbox('', sorted(results_dirs.keys()), index=l
 st.header('Available files and directories for:')
 st.info(results_dir_name)
 st.write([p.name for p in results_dirs[results_dir_name].glob('*')])
-paths = IOPaths('', RESULTS_PATH / results_dir_name)
+paths = ExperimentPaths('', RESULTS_PATH / results_dir_name)
 
 st.header('Discourse Trees Data Frame structure')
-discourse_tree_df_cache = st.cache(serializer.load)
-discourse_tree_df = discourse_tree_df_cache(paths.discourse_trees_df)
-st.write(discourse_tree_df.sample(5))
+# discourse_tree_df_cache = st.cache(serializer.load)
+# discourse_tree_df = discourse_tree_df_cache(paths.discourse_trees_df)
+# st.write(discourse_tree_df.sample(5))
 
 aspect_sentiments = dict(serializer.load(paths.aspect_sentiments))
 st.header('Aspect sentiments')
 # st.write(aspect_sentiments)
 
-arrg = serializer.load(paths.aspects_graph)
-dot = nx.nx_pydot.to_pydot(arrg.subgraph(['quality', 'case']))
+st.write(paths.discourse_trees_df)
+arrg = serializer.load(paths.aspect_hierarchical_tree)
+dot = nx.nx_pydot.to_pydot(arrg)
 st.graphviz_chart(dot.to_string())
