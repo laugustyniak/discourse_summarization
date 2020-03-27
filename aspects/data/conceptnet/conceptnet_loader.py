1802d291e3b3fb109a5a1b64980b4d8716c322b9
Łukasz <luk.augustyniak@gmail.com>
2020-03-27 13:21:14 +0100

    parse conceptnet dump

--- aspects/data/conceptnet/conceptnet_loader.py
+++ aspects/data/conceptnet/conceptnet_loader.py
@@ -17,39 +17,40 @@ CONCEPTNET_LINES_ALL = 34074917
 def parse_conceptnet_dump(concepnet_path: Path) -> Generator:
     with concepnet_path.open("r") as f:
         for line in tqdm(f, total=34074917):
-            relation, node_from, node_to = parse_line(line)
+            relation, node_from, node_to, lang = parse_line(line)
             if relation and node_from and node_to:
-                yield relation, node_from, node_to
+                yield relation, node_from, node_to, lang
 
 
 def parse_line(line_text: str):
     _, relation_text, node_from_text, node_to_text, _ = line_text.split("\t")
-    node_from = parse_conceptnet_node(node_from_text)
+    node_from, lang = parse_conceptnet_node(node_from_text)
     if not node_from:
-        return None, None, None
-    node_to = parse_conceptnet_node(node_to_text)
+        return None, None, None, None
+    node_to, lang = parse_conceptnet_node(node_to_text)
     if not node_to:
-        return None, None, None
+        return None, None, None, None
     relation = parse_conceptnet_relation(relation_text)
     if not relation:
-        return None, None, None
-    return relation, node_from, node_to
+        return None, None, None, None
+    return relation, node_from, node_to, lang
 
 
 def parse_conceptnet_node(node_text: str):
     node_text_parts = node_text.split("/")[1:]
     if node_text_parts[0] != 'c':
         # logger.error(f"node {node_text} starts with other than c prefix - misconception about data structure")
-        return None
-    if node_text_parts[1] not in CONCEPTNET_LANGS:
-        return None
+        return None, None
+    lang = node_text_parts[1]
+    if lang not in CONCEPTNET_LANGS:
+        return None, lang
     if len(node_text_parts) == 3:
-        return node_text_parts[2]
+        return node_text_parts[2], lang
     if len(node_text_parts) == 4:
         # consider returning also POS
-        return node_text_parts[2]
+        return node_text_parts[2], lang
     # logger.error(f"node {node_text} has unexpected strucure - misconception about data structure")
-    return None
+    return None, lang
 
 
 def parse_conceptnet_relation(relation_text: str):
@@ -69,6 +70,6 @@ if __name__ == '__main__':
     conceptnet_path = Path('conceptnet-5.7.0-assertions.csv')
     df = pd.DataFrame(
         list(parse_conceptnet_dump(conceptnet_path)),
-        columns=['relation', 'source', 'target', ]
+        columns=['relation', 'source', 'target', 'lang']
     )
     df.to_csv(conceptnet_path.with_suffix('.en-pl.csv'))
