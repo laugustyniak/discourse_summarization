df6fc544e333136b138a089d33328bc909d9c296
Łukasz <luk.augustyniak@gmail.com>
2020-03-31 10:00:07 +0200

    node names as property in graph tools graph

--- aspects/data/conceptnet/utils.py
+++ aspects/data/conceptnet/utils.py
@@ -2,6 +2,7 @@ from functools import lru_cache
 
 import graph_tool as gt
 import pandas as pd
+from tqdm import tqdm
 
 from aspects.utilities.settings import CONCEPTNET_CSV_EN_PATH, CONCEPTNET_GRAPH_TOOL_EN_PATH
 
@@ -9,15 +10,28 @@ from aspects.utilities.settings import CONCEPTNET_CSV_EN_PATH, CONCEPTNET_GRAPH_
 def generate_english_graph() -> gt.Graph:
     df = pd.read_csv(CONCEPTNET_CSV_EN_PATH, index_col=0)
 
-    graph = gt.Graph()
-    relation = graph.new_edge_property('string')
-    # df.values creates numpy array of edgelist
-    graph.add_edge_list(df.values, hashed=True, eprops=[relation])
+    g = gt.Graph()
+    e_relation = g.new_edge_property('string')
+    v_aspect_name = g.new_vertex_property('string')
+
+    vertices = {}
+    for aspect_name in tqdm(set(df.source.tolist() + df.target.tolist()), desc='Vertices adding to the graph...'):
+        v = g.add_vertex()
+        vertices[aspect_name] = v
+        v_aspect_name[v] = aspect_name
+
+    g.vertex_properties['aspect_name'] = v_aspect_name
+
+    for row in tqdm(df.itertuples(), desc='Edges adding to the graph...', total=len(df)):
+        e = g.add_edge(vertices[row.source], vertices[row.target])
+        e_relation[e] = row.relation
+
+    g.edge_properties['relation'] = e_relation
 
     # serialize
-    graph.save(CONCEPTNET_GRAPH_TOOL_EN_PATH.as_posix())
+    g.save(CONCEPTNET_GRAPH_TOOL_EN_PATH.as_posix())
 
-    return graph
+    return g
 
 
 @lru_cache(1)
