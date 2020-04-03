3e5ecd8d4afce91934b02601c826ec44a8e241ef
Łukasz <luk.augustyniak@gmail.com>
2020-04-03 14:05:00 +0200

    fitlering and preparetion

--- aspects/embeddings/graph/conceptnet_hierarchies_check.py
+++ aspects/embeddings/graph/conceptnet_hierarchies_check.py
@@ -1,9 +1,9 @@
 from typing import NamedTuple, List
 
 import numpy as np
-from graph_tool import Vertex
+from graph_tool import Vertex, Graph
 from graph_tool.draw import graph_draw
-from graph_tool.stats import remove_self_loops
+from graph_tool.stats import remove_self_loops, linspace
 from graph_tool.topology import shortest_distance
 from tqdm import tqdm
 
@@ -34,24 +34,72 @@ def replace_zero_len_paths(shortest_paths: np.array, replaced_value: int = 0) ->
     return np.where(shortest_paths > GRAPH_TOOL_SHORTEST_PATHS_0_VALUE, replaced_value, shortest_paths)
 
 
-if __name__ == '__main__':
-    conceptnet_graph = load_english_graph()
-    remove_self_loops(conceptnet_graph)
-    vertices_conceptnet = dict(zip(conceptnet_graph.vertex_properties['aspect_name'], conceptnet_graph.vertices()))
+def prepare_conceptnet_graph():
+    g = load_english_graph()
+    remove_self_loops(g)
 
     # filter relations
-    e_hierarchical_relation_filter = conceptnet_graph.new_edge_property('bool')
-    relations = list(conceptnet_graph.properties[('e', 'relation')])
+    e_hierarchical_relation_filter = g.new_edge_property('bool')
+    relations = list(g.properties[('e', 'relation')])
     for edge, edge_relation in tqdm(
             zip(
-                conceptnet_graph.edges(),
+                g.edges(),
                 relations
             ),
             desc='Edge filtering...',
             total=len(relations)
     ):
         e_hierarchical_relation_filter[edge] = edge_relation in HIERARCHICAL_RELATIONS
-    conceptnet_graph.set_edge_filter(e_hierarchical_relation_filter)
+    g.set_edge_filter(e_hierarchical_relation_filter)
+
+    vertices = dict(zip(g.vertex_properties['aspect_name'], g.vertices()))
+
+    return g, vertices
+
+
+def intersected_nodes(g1, g2, filter_graphs_to_intersected_vertices: bool = False, property_name: str = 'aspect_name'):
+    print(f'g1: {g1}')
+    print(f'g2: {g2}')
+    g1_nodes = set(g1.vp[property_name])
+    g2_nodes = set(g2.vp[property_name])
+    g1_and_g2 = g1_nodes.intersection(g2_nodes)
+    g1_not_in_g2 = g1_nodes.difference(g2_nodes)
+    print(
+        f'g1 nodes: #{len(g1_nodes)}\n'
+        f'g2 nodes: #{len(g2_nodes)}\n'
+        f'g1 and g2 nodes: #{len(g1_and_g2)}\n'
+        f'g1 not in g2 nodes: #{len(g1_not_in_g2)}\n'
+    )
+
+    if filter_graphs_to_intersected_vertices:
+        v2_intersected = g2.new_vertex_property('bool')
+        for v, v_name in tqdm(zip(g2.vertices(), list(g2.vp[property_name])), desc='Vertices filtering...'):
+            v2_intersected[v] = v_name in g1_and_g2
+        g2.set_vertex_filter(v2_intersected)
+        g2.reindex_edges()
+
+        v1_intersected = g1.new_vertex_property('bool')
+        for v, v_name in tqdm(zip(g1.vertices(), list(g1.vp[property_name])), desc='Vertices filtering...'):
+            v1_intersected[v] = v_name in g1_and_g2
+        g1.set_vertex_filter(v1_intersected)
+        g1.reindex_edges()
+
+    return g1, g2
+
+
+def remove_not_connected_vertices(g):
+    print(f'Pre-filter graph stats: {g}')
+    v_connected = g.new_vertex_property('bool')
+    for v in g.vertices():
+        v_connected[v] = v.in_degree() > 0 or v.out_degree() > 0
+    g.set_vertex_filter(v_connected)
+    g.reindex_edges()
+    print(f'Post-filter graph stats: {g}')
+    return Graph(g, prune=True, directed=g.is_directed())
+
+
+if __name__ == '__main__':
+    conceptnet_graph, vertices_conceptnet = prepare_conceptnet_graph()
 
     experiment_paths = ExperimentPaths(
         input_path='',
@@ -61,6 +109,25 @@ if __name__ == '__main__':
     aspect_graph = serializer.load(experiment_paths.aspect_to_aspect_graph)
     aspect_graph = networkx_2_graph_tool(aspect_graph, node_name_property='aspect_name')
     remove_self_loops(aspect_graph)
+    aspect_graph.reindex_edges()
+
+    aspect_graph, conceptnet_graph = intersected_nodes(
+        g1=aspect_graph,
+        g2=conceptnet_graph,
+        filter_graphs_to_intersected_vertices=False,
+        property_name='aspect_name'
+    )
+
+    conceptnet_graph = remove_not_connected_vertices(conceptnet_graph)
+
+    graph_draw(
+        conceptnet_graph,
+        vertex_font_size=12,
+        output_size=(2000, 2000),
+        output="conceptnet-intersected-aspects.png",
+        bg_color='white',
+        vertex_text=conceptnet_graph.vp.aspect_name
+    )
 
     vertices_aspect_graph = dict(zip(aspect_graph.vertices(), aspect_graph.vertex_properties['aspect_name']))
     vertices_aspect_graph_reverted = dict(zip(aspect_graph.vertex_properties['aspect_name'], aspect_graph.vertices()))
@@ -70,6 +137,7 @@ if __name__ == '__main__':
 
         v_name = vertices_aspect_graph[v]
         if v_name in SEED_ASPECTS:
+            rank = 1
             neighbors = list(set(v.out_neighbors()))
             neighbors_names = [vertices_aspect_graph[neighbor] for neighbor in neighbors]
 
@@ -85,21 +153,25 @@ if __name__ == '__main__':
                             target=vertices_conceptnet[neighbor_name]
                         )
                     )
-                    shortest_paths_aspects.append(1)
+                    shortest_paths_aspects.append(rank)
 
             shortest_paths[v_name] = AspectNeighborhood(
                 vertex=v,
                 name=v_name,
-                rank=1,
+                rank=rank,
                 neighbors_names=neighbors_names,
                 neighbors_path_lens=shortest_paths_aspects,
                 neighbors_cn_path_lens=list(replace_zero_len_paths(np.array(shortest_paths_conceptnet)))
             )
 
+    serializer.save(shortest_paths, experiment_paths.conceptnet_hierarchy_neighborhood)
+
+    d = g.degree_property_map("out", weight)  # weight is an edge property map
+    bins = linspace(d.a.min(), d.a.max(), 40)  # linear bins
+    h = vertex_hist(g, d, bins)
+
     # shortest_paths = shortest_distance(aspect_graph)
     # shortest_paths = np.array(list(shortest_paths))
     # shortest_paths = np.where(shortest_paths > GRAPH_TOOL_SHORTEST_PATHS_0_VALUE, 0, shortest_paths)
-
-    graph_draw(aspect_graph, vertex_font_size=18, output_size=(800, 800), output="aspect-test-graph.png")
 
     print(aspect_graph)
