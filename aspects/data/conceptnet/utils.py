0a2bbac3deba32cfa6c8f174808b982965f304f0
Łukasz <luk.augustyniak@gmail.com>
2020-04-05 21:09:46 +0200

    hierarchical conceptnet

--- aspects/data/conceptnet/utils.py
+++ aspects/data/conceptnet/utils.py
@@ -1,12 +1,37 @@
+from pathlib import Path
+from typing import Union, Set
+
 import graph_tool as gt
 import pandas as pd
 from tqdm import tqdm
 
-from aspects.utilities.settings import CONCEPTNET_CSV_EN_PATH, CONCEPTNET_GRAPH_TOOL_EN_PATH
+from aspects.utilities.settings import CONCEPTNET_CSV_EN_PATH, CONCEPTNET_GRAPH_TOOL_HIERARCHICAL_EN_PATH
+
+NUCLEUS_SATELLITE_RELATIONS = {
+    'ReceivesAction',
+    'HasA',
+    'UsedFor',
+    'CapableOf',
+    'MadeOf',
+    'HasSubevent',
+    'CreatedBy',
+    'DefinedAs',
+}
+SATELLITE_NUCLEUS_RELATIONS = {
+    'PartOf',
+    'IsA',
+    'MannerOf',
+    'AtLocation',
+    'Causes',
+    'DerivedFrom',
+}
 
 
-def generate_english_graph() -> gt.Graph:
+def generate_english_graph(graph_path: Union[str, Path], relation_types: Set[str] = None) -> gt.Graph:
     df = pd.read_csv(CONCEPTNET_CSV_EN_PATH, index_col=0)
+
+    if relation_types is not None:
+        df = df[df.relation.isin(relation_types)]
 
     g = gt.Graph()
     e_relation = g.new_edge_property('string')
@@ -27,13 +52,20 @@ def generate_english_graph() -> gt.Graph:
     g.edge_properties['relation'] = e_relation
 
     # serialize
-    g.save(CONCEPTNET_GRAPH_TOOL_EN_PATH.as_posix())
+    g.save(str(graph_path))
 
     return g
 
 
-def load_english_graph() -> gt.Graph:
-    if CONCEPTNET_GRAPH_TOOL_EN_PATH.exists():
-        return gt.load_graph(CONCEPTNET_GRAPH_TOOL_EN_PATH.as_posix())
+def load_english_hierarchical_graph() -> gt.Graph:
+    if CONCEPTNET_GRAPH_TOOL_HIERARCHICAL_EN_PATH.exists():
+        return gt.load_graph(CONCEPTNET_GRAPH_TOOL_HIERARCHICAL_EN_PATH.as_posix())
     else:
-        return generate_english_graph()
+        return generate_english_graph(
+            graph_path=CONCEPTNET_GRAPH_TOOL_HIERARCHICAL_EN_PATH,
+            relation_types=SATELLITE_NUCLEUS_RELATIONS.union(NUCLEUS_SATELLITE_RELATIONS)
+        )
+
+
+if __name__ == '__main__':
+    load_english_hierarchical_graph()
