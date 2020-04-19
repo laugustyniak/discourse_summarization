3878f005c565a22aa55519ba25368f3dbda304ae
Łukasz <luk.augustyniak@gmail.com>
2020-04-19 13:34:25 +0200

    two version calculate

--- aspects/examples/run_specific_dataset.py
+++ aspects/examples/run_specific_dataset.py
@@ -1,14 +1,23 @@
 from aspects.pipelines.aspect_analysis import AspectAnalysis
 from aspects.utilities import settings
 
-DATASET_PATH = settings.AMAZON_REVIEWS_CELL_PHONES_AND_ACCESSORIES_DATASET_JSON
+DATASET_PATH = settings.AMAZON_REVIEWS_AMAZON_INSTANT_VIDEO_DATASET_JSON
 
-aspect_analysis = AspectAnalysis(
+# TODO: better parametrization for experiments
+aspect_analysis_gerani = AspectAnalysis(
     input_path=DATASET_PATH.as_posix(),
     output_path=settings.DEFAULT_OUTPUT_PATH / DATASET_PATH.stem,
+    experiment_name='gerani',
     jobs=2,
     batch_size=100,
-    max_docs=50000
 )
+aspect_analysis_gerani.gerani_pipeline()
 
-aspect_analysis.gerani_pipeline()
+aspect_analysis_our = AspectAnalysis(
+    input_path=DATASET_PATH.as_posix(),
+    output_path=settings.DEFAULT_OUTPUT_PATH / DATASET_PATH.stem,
+    experiment_name='our',
+    jobs=2,
+    batch_size=100,
+)
+aspect_analysis_our.our_pipeline()
