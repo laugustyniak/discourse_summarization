886dbd8a66c6744a65d87f8c72cef2e1d0d988c5
Łukasz Augustyniak <luk.augustyniak@gmail.com>
2020-07-20 02:07:19 +0200

    update repo naming

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="sentiment-aspects-analyzer",
+    name="discourse_summarization",
     version="0.0.1",
     author="Lukasz Augustyniak, Krzysztof Rajda",
     author_email="lukasz.augustyniak@pwr.edu.pl, krzysztof.rajda@pwr.edu.pl",
     description="The repository with source code devoted to aspect-based sentiment analysis",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/laugustyniak/sentiment-aspect-analyzer",
+    url="https://github.com/laugustyniak/discourse_summarization",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
