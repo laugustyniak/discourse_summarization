87af394f63563cac8c1b43bb3af5f5fc05398a1b
Łukasz Augustyniak <luk.augustyniak@gmail.com>
2020-09-09 13:38:28 +0200

    fix header

--- docker/restart.sh
+++ docker/restart.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#/bin/bash
 
 docker-compose stop
 docker-compose rm -f
--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
+#/bin/bash
 docker run -p 8877:8888 -it -u user -w /home/user -v $(pwd):/home/user discourse_summarization bash
