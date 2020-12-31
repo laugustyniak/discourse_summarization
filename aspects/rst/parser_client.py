1627ff542396d300b16ff1250c516686045d7b33
Łukasz Augustyniak <luk.augustyniak@gmail.com>
2020-12-31 03:37:56 +0100

    update rst client

--- aspects/rst/parser_client.py
+++ aspects/rst/parser_client.py
@@ -1,19 +1,13 @@
 import requests
-
+import uuid
 from aspects.utilities.settings import RST_PARSER_DOCKER_URL
 
 
 class RSTParserClient:
-
-    def __init__(self, url=None, json_request_key=None, json_response_key=None):
+    def __init__(self, url=None):
         self.url = url or RST_PARSER_DOCKER_URL
-        self.json_request_key = json_request_key or 'text'
-        self.json_response_key = json_response_key or 'tree'
 
-    def parse(self, text):
-        return requests.post(
-            self.url,
-            json={
-                self.json_request_key: text
-            }
-        ).json()[self.json_response_key]
+    def parse(self, text: str) -> str:
+        files = {"input": (f"{str(uuid.uuid4())}.txt", text)}
+        response = requests.post(self.url, files=files)
+        return response.text
