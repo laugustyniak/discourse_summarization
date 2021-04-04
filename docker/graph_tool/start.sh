00e072dc1facb6c4551cf7b7d4bb471a06af2707
Łukasz Augustyniak <luk.augustyniak@gmail.com>
2021-04-04 13:50:38 +0200

    docker run commands

--- docker/graph_tool/start.sh
+++ docker/graph_tool/start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# linux
 docker run -p 8877:8888 -p 6006:6006 -it -u user -w /home/user -v /datasets/sentiment/aspects:/home/user graph_tool bash
+# windows
+# docker run -p 8877:8888 -p 6006:6006 -it -u user -w /home/user -v E:/github/phd/sentiment-backend:/home/user graph_tool bash
 
 # then run in container -> jupyter notebook --ip 0.0.0.0
